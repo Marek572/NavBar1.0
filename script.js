9d3c715f68f10aa707684c05ca3b24c8db300fa6
Marek Porębski <marek.bp@poczta.fm>
2021-08-17 10:52:23 +0200

    adjust onclick

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // onclick color change 
+//czy da sie jakos lety/vary/const wstawic w funkcje colorchange w miejsce document.element???
+
+let colorChange = document.getElementById('adjust');
+
+colorChange.addEventListener('click', () => {
+    document.body.classList.toggle('orange');
+    document.getElementById('nav').classList.toggle('red');
+    document.getElementById('home').classList.toggle('white');
+    document.getElementById('profile').classList.toggle('white');
+    document.getElementById('contact').classList.toggle('white');
+    document.getElementById('adjust').classList.toggle('white');
+    document.getElementById('settings').classList.toggle('white');
+})
